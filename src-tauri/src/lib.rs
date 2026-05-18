f2408a2649e2eef53e1d5c85a0220571cc7cba04
9yCrooked <3444364899@qq.com>
2026-05-18 13:57:12 +0800

    chore: configure tauri updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -194,6 +194,8 @@ struct BackupMeta {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             import_accounts,
             list_accounts,
